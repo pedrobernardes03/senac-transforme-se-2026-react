6c1f0c30a1cddc68bf93a64cc62428049361ee94
pedrobernardes03 <pedro17022003@gmail.com>
2026-09-02 20:13:15 -0300

    batatinha didatica

--- curso-react/src/pages/Auth.jsx
+++ curso-react/src/pages/Auth.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import { Link } from 'react-router'
 function Auth(){
+    /* const[variavel, funcaoAlteraVariavel] = useState('valor inicial'); */
+    const[batatinha, setBatatinha]= useState(2);
+    function sub(){
+        setBatatinha(batatinha - 1)
+    }
     return(
-        <div>
+        <div className=''>
     <Link to="/" className="py-2 px-3 bg-primary text-white rounded-md hover:shadow-inner ml-auto mr-4 shadow">Voltar</Link>
+    <div className="bg-red-100 rounded-full p-2" onClick={sub}>-</div>
+    {batatinha}
+    <div className="bg-green-100 rounded-full p-2" onClick={() => setBatatinha(batatinha + 1)}>+</div>
     <div className="h-full flex flex-col">
-        <div className="max-w-sm mx-auto my-auto p-5 bg-blue1 rounded-lg shadow-md flex flex-col items-center">
+        <div className="mx-auto my-auto p-5 bg-blue1 rounded-lg shadow-md flex flex-col items-center">
             <h2 className="text-primary">Login</h2>
             
             <form>
--- curso-react/src/pages/Painel.jsx
+++ curso-react/src/pages/Painel.jsx
@@ -4,11 +4,11 @@ function Painel(){
     <h3 id="welcome"></h3>
     <div id="modalRegister"
         className="fixed top-0 right-0 bottom-0 left-0 hidden items-center justify-center z-50 bg-black-50">
-        <div className="relative max-w-sm w-full p-5 bg-about rounded-lg shadow-md flex column">
+        <div className="relative max-w-sm w-full p-5 bg-about rounded-lg shadow-md flex flex-col">
             <a id="close" className="bg-red text-white absolute top-0 right-0 px-2 rounded-full cursor-pointer">X</a>
             <h2>Cadastro de Usuário</h2>
             <p>Preencha as informações abaixo</p>
-            <form className="flex column">
+            <form className="flex flex-col">
                 Nome: <input id="iName" type="text" placeholder="Digite seu nome completo" />
                 Email: <input id="iEmail" type="email" placeholder="Digite o seu melhor email" />
                 Senha: <input id="iPassword" type="password" placeholder="Letra maiuscula e números" />
