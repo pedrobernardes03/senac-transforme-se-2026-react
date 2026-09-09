cc1cd55654753dcacab466069962dd8991d37a2f
pedrobernardes03 <pedro17022003@gmail.com>
2026-09-09 18:42:00 -0300

    corrigi o erro que estava dando no console e formatei a mensagem

--- curso-react/src/pages/Auth.jsx
+++ curso-react/src/pages/Auth.jsx
@@ -14,8 +14,8 @@ function Auth(){
         let user = users.find(u=>{
             return u.email == email
         })
-        if(!users){
-            setMsg("email não cadastrado")
+        if(!user){
+            setMsg("Email não cadastrado")
             return
         }
         if(user.senha == password){
@@ -23,8 +23,8 @@ function Auth(){
             localStorage.setItem("logado", JSON.stringify(user))
             nav("/painel")
         }else{
-            setMsg("senha incorreta")
-
+            setMsg("Senha incorreta")
+            return
         }
     }
 
@@ -32,17 +32,17 @@ function Auth(){
         <div className='w-1/4 mx-auto my-auto p-8 bg-blue-100 rounded-lg shadow-md flex flex-col'>
            
             <form className='flex flex-col mt-3'>
-                <span>{msg}</span>
+                <span className='text-red-600'>{msg}</span>
                 <h2 className="text-primary text-3xl"> <b>Login</b> </h2>
                 
                 
                 <span className='mt-3 text-lg text-blue-900'>
                     <b>Email:</b> <input className="bg-white placeholder-blue-900" type="email" placeholder="Digite o seu email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    {email}
+                    
                 </span>
                 <span className="mt-3 text-lg text-blue-900">
                     <b>Senha:</b> <input className="bg-white text-lg placeholder-blue-900" type="password" placeholder="Digite sua senha" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    {password}
+                    
                 </span>
                  <div className='grid grid-cols-2 mt-3'>
                  <Link to="/" className="mx-auto p-1 bg-primary text-lg text-white rounded-md hover:shadow-inner shadow items-center ">Voltar</Link>
